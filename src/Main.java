54b0f763d919790ad99a0deb588100bcb86be5c0
Danilo Silva <jdsc@cin.ufpe.br>
2021-11-08 15:27:55 -0300

    Removing song

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         new Player();
-        // adicionar e remover músicas da lista de reprodução, play/pause
+        // play/pause
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ import ui.PlayerWindow;
 import ui.AddSongWindow;
 
 import java.awt.event.*;
+import java.util.Objects;
 
 public class Player {
     private final PlayerWindow window;
@@ -12,11 +13,11 @@ public class Player {
     public Player() {
         String windowTitle = "Spotifraco";
 
-        ActionListener playNowAction = e -> System.out.println("Clicou no playNow!");
-        ActionListener RemoveAction = e -> System.out.println("Clicou no Remove!");
+        ActionListener playNowAction = e -> playSong();
+        ActionListener RemoveAction = e -> removeSong();
         ActionListener AddSongAction = e -> addSong();
         ActionListener PlayPauseAction = e -> System.out.println("Clicou no PlayPause!");
-        ActionListener StopAction = e -> System.out.println("Clicou no Stop!");
+        ActionListener StopAction = e -> stopSong();
         ActionListener NextAction = e -> System.out.println("Clicou no Next!");
         ActionListener PreviousAction = e -> System.out.println("Clicou no Previous!");
         ActionListener ShuffleAction = e -> System.out.println("Clicou no Shuffle!");
@@ -75,6 +76,19 @@ public class Player {
                 queueArray
         );
     }
+
+    private void addSongToQueue(String[] newSong) {
+        String[][] newQueueList = new String[queueArray.length + 1][7];
+        System.arraycopy(
+                queueArray, 0,
+                newQueueList, 0,
+                queueArray.length
+        );
+        newQueueList[queueArray.length] = newSong;
+        window.updateQueueList(newQueueList);
+        this.queueArray = newQueueList;
+    }
+
     private void addSong() {
         ActionListener addSongOkAction = e -> {
             String[] array = currentAddSongWindow.getSong();
@@ -87,19 +101,36 @@ public class Player {
                 listener
         );
         this.currentAddSongWindow = newWindow;
-        newWindow.start();
         this.currentSongID ++;
+        newWindow.start();
     }
 
-    private void addSongToQueue(String[] newSong) {
-        String[][] newQueueList = new String[queueArray.length + 1][7];
-        System.arraycopy(
-                queueArray, 0,
-                newQueueList, 0,
-                queueArray.length
-        );
-        newQueueList[queueArray.length] = newSong;
-        window.updateQueueList(newQueueList);
+    private void removeSong() {
+        int id = this.window.getSelectedSongID();
+        String[][] newQueueList = new String[queueArray.length - 1][7];
+        for (int index = 0; index < queueArray.length; index++) {
+            String[] song = queueArray[index];
+            if (!Objects.equals(song[6], Integer.toString(id))) {
+                newQueueList[index] = song;
+            }
+        }
+        this.queueArray = newQueueList;
+        this.window.updateQueueList(newQueueList);
+    }
+
+    private void playSong() {
+        int id = this.window.getSelectedSongID();
+        for (String[] song: queueArray) {
+            if (Objects.equals(song[6], Integer.toString(id))) {
+                this.window.updatePlayingSongInfo(
+                        song[0], song[1], song[2]);
+            }
+        }
+        this.window.enableScrubberArea();
+    }
+
+    private void stopSong() {
+        this.window.resetMiniPlayer();
     }
 }
 
